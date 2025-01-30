03be8796d1b9578ac4b6a11870b059d46ab2c218
しゅみ君 \ Syumikun. <78669011+YukiShima1010@users.noreply.github.com>
2025-01-30 10:54:08 +0900

    エラーページをUP

---
+++ error/403.php
@@ -0,0 +1,49 @@
+<?php
+
+// config読み込み
+require_once __DIR__ . "/../config/config.php";
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>アクセス権限がありません | みーつけた</title>
+    <meta name="robots" content="noindex">
+
+    <!-- アイコン -->
+    <link rel="icon" href="https://<?php echo $config["domain"]; ?>/assets/img/favicon.ico"/>
+    <link rel="apple-touch-icon" href="https://<?php echo $config["domain"]; ?>/assets/img/apple-touch-icon.png"/>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://<?php echo $config["domain"]; ?>/error/error.css" type="text/css">
+</head>
+<body>
+
+    <!-- ヘッダー -->
+    <header>
+        <a href="https://<?php echo $config["domain"]; ?>"><h1>みーつけた</h1></a>
+    </header>
+
+    <!-- コンテンツ -->
+    <div class="content">
+
+        <!-- コンテナ -->
+        <div class="container">
+
+            <h1>403</h1>
+            <h2>アクセス権限がありません</h2>
+            <button onclick='window.location = "https://<?php echo $config["domain"]; ?>"'>ホームへ</button>
+
+        </div>
+
+    </div> <!-- contents -->
+
+    <!-- フッター -->
+    <footer>
+        <p>&copy; 2025 YukiShima</p>
+    </footer>
+
+</body>
+</html>
---
+++ error/recaptcha.php
@@ -0,0 +1,49 @@
+<?php
+
+// config読み込み
+require_once __DIR__ . "/../config/config.php";
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>reCAPTCHA認証に失敗しました | みーつけた</title>
+    <meta name="robots" content="noindex">
+
+    <!-- アイコン -->
+    <link rel="icon" href="https://<?php echo $config["domain"]; ?>/assets/img/favicon.ico"/>
+    <link rel="apple-touch-icon" href="https://<?php echo $config["domain"]; ?>/assets/img/apple-touch-icon.png"/>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://<?php echo $config["domain"]; ?>/error/error.css" type="text/css">
+</head>
+<body>
+
+    <!-- ヘッダー -->
+    <header>
+        <a href="https://<?php echo $config["domain"]; ?>"><h1>みーつけた</h1></a>
+    </header>
+
+    <!-- コンテンツ -->
+    <div class="content">
+
+        <!-- コンテナ -->
+        <div class="container">
+
+            <h1>失敗</h1>
+            <h2>reCAPTCHA認証に失敗しました</h2>
+            <button onclick='window.location = "https://<?php echo $config["domain"]; ?>"'>ホームへ</button>
+
+        </div>
+
+    </div> <!-- contents -->
+
+    <!-- フッター -->
+    <footer>
+        <p>&copy; 2025 YukiShima</p>
+    </footer>
+
+</body>
+</html>
